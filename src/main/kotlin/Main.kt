94a9e1c188f0d0c94fa6d555cdb1adbfa606477b
Muhammed Didin <mdidin80@gmail.com>
2022-02-20 13:54:07 +0300

    Null safety, assigning values in if statements, swapping values, return on when statements are implemented.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -26,4 +26,51 @@ fun main(args: Array<String>) {
     for ((x, y) in map) {
         print("$x -> $y  ")
     }
+    println()
+    //Iterating a list.
+    for (i in 1..10) {  // includes 10
+        print(i)
+    }
+    println()
+    for (i in 1 until 10) { //does not include 10
+        print(i)
+    }
+    println()
+    for (x in 2..10 step 2) { //step by step looping, includes 2 and 10
+        print(x)
+    }
+    println()
+
+    //null safety.
+    print("Enter a message: ")
+    /*
+    val s = readLine()
+    println(s?.length) //prints the length of s if s is not null.
+     */
+    println(transform(("Red")))
+    //assigning values in if statements.
+    val x = 2
+    val y = if (x == 1) {
+        "one"
+    } else if (x == 2) {
+        "two"
+    } else {
+        "other"
+    }
+    println(y)
+    //swapping values
+    var a = 1
+    var b = 2
+    a = b.also { b = a }
+    println("a: $a b:$b")
+}
+//return on when conditioning. Returns special values for each color.
+fun transform(color: String): Int {
+    return when (color) {
+        "Red" -> 0
+        "Green" -> 1
+        "Blue" -> 2
+        else -> throw IllegalArgumentException("Invalid color param value") //throws an exception in the else statement.
+
+    }
 }
